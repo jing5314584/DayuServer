e7839dbe583b95859c16457c46f72c3b286c8e53
jing5314584 <1311821013@qq.com>
2020-06-23 13:48:27 +0800

    First

---
+++ src/main/java/com/dayu/server/mbg/mapper/BrandMapper.java
@@ -0,0 +1,30 @@
+package com.dayu.server.mbg.mapper;
+
+import com.dayu.server.mbg.model.Brand;
+import com.dayu.server.mbg.model.BrandExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface BrandMapper {
+    long countByExample(BrandExample example);
+
+    int deleteByExample(BrandExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Brand record);
+
+    int insertSelective(Brand record);
+
+    List<Brand> selectByExample(BrandExample example);
+
+    Brand selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Brand record, @Param("example") BrandExample example);
+
+    int updateByExample(@Param("record") Brand record, @Param("example") BrandExample example);
+
+    int updateByPrimaryKeySelective(Brand record);
+
+    int updateByPrimaryKey(Brand record);
+}
---
+++ src/main/java/com/dayu/server/mbg/model/Brand.java
@@ -0,0 +1,111 @@
+package com.dayu.server.mbg.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Brand implements Serializable {
+    private Integer id;
+
+    private String brandName;
+
+    private String brandIcon;
+
+    private String brandDesc;
+
+    private Integer brandPlances;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private Boolean isShow;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName == null ? null : brandName.trim();
+    }
+
+    public String getBrandIcon() {
+        return brandIcon;
+    }
+
+    public void setBrandIcon(String brandIcon) {
+        this.brandIcon = brandIcon == null ? null : brandIcon.trim();
+    }
+
+    public String getBrandDesc() {
+        return brandDesc;
+    }
+
+    public void setBrandDesc(String brandDesc) {
+        this.brandDesc = brandDesc == null ? null : brandDesc.trim();
+    }
+
+    public Integer getBrandPlances() {
+        return brandPlances;
+    }
+
+    public void setBrandPlances(Integer brandPlances) {
+        this.brandPlances = brandPlances;
+    }
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Boolean getIsShow() {
+        return isShow;
+    }
+
+    public void setIsShow(Boolean isShow) {
+        this.isShow = isShow;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", brandName=").append(brandName);
+        sb.append(", brandIcon=").append(brandIcon);
+        sb.append(", brandDesc=").append(brandDesc);
+        sb.append(", brandPlances=").append(brandPlances);
+        sb.append(", startTime=").append(startTime);
+        sb.append(", endTime=").append(endTime);
+        sb.append(", isShow=").append(isShow);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
